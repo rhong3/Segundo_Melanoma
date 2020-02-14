162bf8de9271b917bf9c77dd4b813a4c9da8df8f
rh2740 <rh2740@nyu.edu>
2020-02-14 10:33:43 -0500

    2.14.1

--- Scripts/ML_prep.R
+++ Scripts/ML_prep.R
@@ -5,22 +5,17 @@
 #' @author: RH
 
 sig = read.csv("~/documents/Segundo_Melanoma/Results/OLA_summary.csv")
-clinical.p = read.csv("~/documents/Segundo_Melanoma/Results/proteomics/OLA/5yr-survival-clinical.csv")
-clinical.p$survival = as.numeric(clinical.p$os.days>1825)
-clinical.p = clinical.p[,c(1,44)]
-clinical.h = read.csv("~/documents/Segundo_Melanoma/Results/phospho/OLA/5yr-survival-clinical.csv")
-clinical.h$survival = as.numeric(clinical.h$os.days>1825)
-clinical.h = clinical.h[,c(1,44)]
-clinical.t = read.csv("~/documents/Segundo_Melanoma/Results/transcriptomics/OLA/5yr-survival-clinical.csv")
-clinical.t$survival = as.numeric(clinical.t$os.days>1825)
-clinical.t = clinical.t[,c(1,44)]
+sig = sig[sig$Feature=="6-month-survival", ]
+clinical.p = read.csv("~/documents/Segundo_Melanoma/Results/proteomics/OLA/6-month-survival/6-month-survival-clinical.csv")
+clinical.h = read.csv("~/documents/Segundo_Melanoma/Results/phospho/OLA/6-month-survival/6-month-survival-clinical.csv")
+clinical.t = read.csv("~/documents/Segundo_Melanoma/Results/transcriptomics/OLA/6-month-survival/6-month-survival-clinical.csv")
 
 clinical = merge(clinical.p, clinical.h)
 clinical = merge(clinical, clinical.t)
 
-pdata = read.csv("~/documents/Segundo_Melanoma/Results/proteomics/OLA/ola_data.csv")
-tdata = read.csv("~/documents/Segundo_Melanoma/Results/transcriptomics/OLA/ola_data.csv")
-hdata = read.csv("~/documents/Segundo_Melanoma/Results/phospho/OLA/ola_data.csv")
+pdata = read.csv("~/documents/Segundo_Melanoma/Results/proteomics/OLA/6-month-survival/ola_data.csv")
+tdata = read.csv("~/documents/Segundo_Melanoma/Results/transcriptomics/OLA/6-month-survival/ola_data.csv")
+hdata = read.csv("~/documents/Segundo_Melanoma/Results/phospho/OLA/6-month-survival/ola_data.csv")
 data.p = pdata[pdata$Accession %in% sig$Accession, ]
 data.t = tdata[tdata$Gene.name %in% sig$Gene.name, ]
 data.h = hdata[hdata$Modified_sequence %in% sig$Modified_sequence, ]
@@ -38,9 +33,9 @@ data.h = t(data.h)
 data.h = data.h[rownames(data.h) %in% clinical$X, ]
 rownames(clinical) = clinical$X
 dataa = cbind(data.p, data.t, data.h, clinical)
-dataa = dataa[,-20]
+dataa = dataa[,-c(122:165)]
 
-write.csv(dataa, "~/documents/Segundo_Melanoma/Results/ML/5yr-survival.csv", row.names=TRUE)
+write.csv(dataa, "~/documents/Segundo_Melanoma/Results/ML/6-month-survival.csv", row.names=TRUE)
 
 
 
