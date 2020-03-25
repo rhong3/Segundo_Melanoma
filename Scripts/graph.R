55b39e0005807ab2a180f322e5b84857b98a645a
HONGRUNYU <michaelhong1995@gmail.com>
2020-03-25 17:43:17 -0400

    3.25.2

--- Scripts/graph.R
+++ Scripts/graph.R
@@ -2,6 +2,14 @@
 library(igraph)
 library(dplyr)
 
+# Pathway, IC, clinical
+Relation <- read.delim("~/Documents/Segundo_Melanoma/Results/ReactomePathwaysRelation.txt", header=FALSE)
+Pathways <- read.delim("~/Documents/Segundo_Melanoma/Results/ReactomePathways.txt", header=FALSE)
+Pathways = Pathways[Pathways['V3'] == 'Homo sapiens', c(1,2)]
+xxxx = na.omit(left_join(Relation, Pathways, by="V1"))[, c(2,3)]
+colnames(xxxx) = c('V1', 'V2')
+xxxx = na.omit(left_join(xxxx, Pathways, by="V1"))[, c(2,3)]
+
 for (a in c('strict', 'relax')){
   summ = read.csv(paste("~/Documents/Segundo_Melanoma/Results/", a, "_ICA_GSEA_summary.csv", sep=''))
   summ['-logp'] = -log(summ$padj)
@@ -21,10 +29,16 @@ for (a in c('strict', 'relax')){
     unq = rbind(unqp, unqc, unqi)
     colnames(summ1) = c("from", "to", "-logp")
     colnames(summ2) = c("from", "to", "-logp")
-    summall = rbind(summ1, summ2)
+    
+    xxx = xxxx[xxxx$V2.y %in% unqp$name & xxxx$V2.x %in% unqp$name,]
+    xxx = xxx[, c(2,1)]
+    colnames(xxx) = c('from', 'to')
+    xxx['-logp'] = mean(summ1$`-logp`)
+    
+    summall = rbind(summ1, summ2, xxx)
     summall = unique(summall)
     g <- graph_from_data_frame(summall, directed=TRUE, vertices=unq)
-    g <- simplify(g, remove.multiple = F, remove.loops = F)
+    g <- simplify(g, remove.multiple = F, remove.loops = T)
     colrs <- c("tomato", "gold", "light blue")
     V(g)$group = gsub("pathway", "light blue", V(g)$group)
     V(g)$group = gsub("clinical", "tomato", V(g)$group)
@@ -49,5 +63,3 @@ for (a in c('strict', 'relax')){
     dev.off()
   }
 }
-
-
