1a91a7c24b2d9ba8966ff1ed232ebb67b9902dd9
HONGRUNYU <michaelhong1995@gmail.com>
2020-04-05 18:35:31 -0400

    4.5.1

--- Scripts/density.R
+++ Scripts/density.R
@@ -1,20 +1,24 @@
 # Make density plots
 library(ggplot2)
 library(reshape2)
+# prot <- read.csv("~/documents/Segundo_Melanoma/Data/proteomics/proteomics.csv")
+# phos <- read.csv("~/documents/Segundo_Melanoma/Data/phospho/phospho.csv")
+
 prot <- read.csv("/beegfs/rh2740/Fenyo/Lund_Melanoma/Data/proteomics/proteomics.csv")
 phos <- read.csv("/beegfs/rh2740/Fenyo/Lund_Melanoma/Data/phospho/phospho.csv")
 rownames(prot) = prot$Accession
 rownames(phos) = phos$Modified_sequence
-prot = t(prot[,-c(112:113)])
-phos = t(phos[,-c(2:4)])
+prot = prot[,-c(112:113)]
+phos = phos[,-c(2:4)]
 
 prot.melt = melt(prot, id.vars = "Accession")
-p <- ggplot(aes(x=value, colour=Var1), data=prot.melt)
+prot.melt = prot.melt[,c(1,3)]
+p <- ggplot(aes(x=value, colour=variable), data=prot.melt)
 p + geom_density()
 ggsave('/beegfs/rh2740/Fenyo/Lund_Melanoma/Results/prot_Density.png', scale = 1, width = 30, height = 12, units = "in", limitsize = TRUE)
 
 phos.melt = melt(phos, id.vars = "Modified_sequence")
-h <- ggplot(aes(x=value, colour=Var1), data=phos.melt)
+h <- ggplot(aes(x=value, colour=variable), data=phos.melt)
 h + geom_desity()
 ggsave('/beegfs/rh2740/Fenyo/Lund_Melanoma/Results/phos_Density.png', scale = 1, width = 30, height = 12, units = "in", limitsize = TRUE)
 
