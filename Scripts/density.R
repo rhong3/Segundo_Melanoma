bce0cd64b2769f24611e79b17999fed1430a3af2
HONGRUNYU <michaelhong1995@gmail.com>
2020-04-05 18:42:22 -0400

    4.5.2

--- Scripts/density.R
+++ Scripts/density.R
@@ -12,7 +12,6 @@ prot = prot[,-c(112:113)]
 phos = phos[,-c(2:4)]
 
 prot.melt = melt(prot, id.vars = "Accession")
-prot.melt = prot.melt[,c(1,3)]
 p <- ggplot(aes(x=value, colour=variable), data=prot.melt)
 p + geom_density()
 ggsave('/beegfs/rh2740/Fenyo/Lund_Melanoma/Results/prot_Density.png', scale = 1, width = 30, height = 12, units = "in", limitsize = TRUE)
